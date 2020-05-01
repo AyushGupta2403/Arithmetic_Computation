699916e8091030fbaa6aad49f2eb0389aa9df3dc
Ayush <ayushmanojgupta24@gmail.com>
2020-05-01 07:37:27 +0530

    Accepting three inputs from user in ArithmeticOperation.sh

--- ArithmaticOperation.sh
+++ ArithmaticOperation.sh
@@ -1,1 +1,6 @@
-echo "Welcome"
+#!/bin/bash -x
+#Accepting three inputs for performing operation
+read -p "Enter the first input" a
+read -p "Enter the second input" b
+read -p "Enter the third input" c
+
